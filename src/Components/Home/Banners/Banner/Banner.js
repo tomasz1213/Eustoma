7b37595263b88108ce4dccca40aa481e94caa5ff
Tomasz Mlyczynski <tomasz141312@gmail.com>
2021-11-09 21:29:19 +0100

    Style fix

--- src/Components/Home/Banners/Banner/Banner.js
+++ src/Components/Home/Banners/Banner/Banner.js
@@ -3,8 +3,7 @@ import classes from './Banner.module.css';
 import imgBanner from '../../../../img/firstBackground.webp';
 
 const banner = () => (
-    <div className={classes.Banner}>
-        <img src={imgBanner} alt="logo"></img>
+    <div style={{backgroundImage:`url(${imgBanner})`}} className={classes.Banner}>
         <p>Pozwól nam ...</p>
         <p>SPRAWIĆ BY, TWÓJ DZIEŃ BYŁ JESZCZE PIĘKNIEJSZY</p>
     </div>

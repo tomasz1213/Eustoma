5f662beef621757a9ce6a56783f6e40e2bbc0eb3
root <root@pop-os.localdomain>
2021-12-21 19:48:26 +0100

    Add styles for Calendar

--- src/Components/Calendar/Calendar.js
+++ src/Components/Calendar/Calendar.js
@@ -1,19 +1,27 @@
 import React, { useState } from "react";
-import classes from "./Calendar.module.scss";
 import CalendarJS from "react-calendar";
+import Menu from "../Menu/Menu";
+import Footer from "../Footer/Footer";
+import classes from "./Calendar.module.scss";
+import "./Calendar.css";
 
 const Calendar = () => {
 	const [value, onChange] = useState(new Date());
 	return (
-		<div className={classes.Calendar}>
-			<CalendarJS
-				onChange={(value, event) => {
-					onChange(value);
-					console.log(value);
-				}}
-				value={value}
-			/>
-		</div>
+		<>
+			<Menu/>
+			<div className={classes.Calendar}>
+				<CalendarJS
+					onChange={(value, event) => {
+						onChange(value);
+						console.log(value);
+					}}
+					value={value}
+					locale="pl-PL"
+				/>
+			</div>
+			<Footer/>
+		</>
 	);
 };
 
